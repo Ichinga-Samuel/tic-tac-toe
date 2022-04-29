e45d4b143dc6203164428db535a2d369b2de1503
Ichinga Samuel <ichingasamuel@gmail.com>
2022-04-29 13:39:48 +0100

    Made some modifications

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 let firstMove = true;
 let currentPlayer;
 let gameButtons = document.querySelectorAll("#game-grid button");
-
+console.log(gameButtons)
 function playMove() {
+  console.log(this.innerText)
   // choose a player at random for the first move
   if(firstMove) {
     currentPlayer = Math.round(Math.random()) === 0 ? 'O' : 'X';
     firstMove = false;
   }
   else {
-    if(this.innerText)
-      return;
+    if(this.innerText) return;
+    console.log("Will not reach here")
     currentPlayer = currentPlayer === 'O' ? 'X' : 'O';
   }
 
@@ -19,6 +20,13 @@ function playMove() {
 
 gameButtons.forEach(button => button.onclick = playMove);
 
-document.querySelector("#replay-button").onclick = () => {
+document.querySelector("#replay-button").onclick = replay
+// () => {
+//   console.log(this)
+//   gameButtons.forEach(button => button.innerText = '');
+// };
+
+function replay(){
+  console.log(this.innerText)
   gameButtons.forEach(button => button.innerText = '');
-};
+}
